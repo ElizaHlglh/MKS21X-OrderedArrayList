f6b0bc01b3af8d2a519bca81fcfe7d0d8346b996
ElizaHlglh <ewu4@stuy.edu>
2018-11-01 23:57:05 -0400

    OrderedArrayList.java

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,5 +6,21 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
   }
-  
+
+  public T set(int index, T value) {
+    return super.set(index,value);
+  }
+
+  public boolean add(T value){
+    return super.add(value);
+  }
+
+  public void add(int index, T value){
+    return super.add(index,value);
+  }
+
+  public T MaxLoc(T value){
+    return 0;
+  }
+
 }
