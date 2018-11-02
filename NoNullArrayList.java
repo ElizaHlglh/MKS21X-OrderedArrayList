eca8c662babdb0ca1a1b711052fd4b10e8fa6ab7
ElizaHlglh <ewu4@stuy.edu>
2018-11-02 01:46:14 -0400

    OrderedArrayList.java

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -8,29 +8,40 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   }
 
   public T set(int index, T value) {
-    if (value.equals(null)) {
+    if (value == null) {
       throw new IllegalArgumentException("Can't set an element as null");
     }
-    else {
+    try {
       return super.set(index,value);
+    }
+    catch(IllegalArgumentException e){
+      System.out.println("Error occur with setting null.");
+      return null;
     }
   }
 
   public boolean add(T value){
-    if (value.equals(null)) {
+    if (value == null) {
       throw new IllegalArgumentException("Can't add null");
     }
-    else{
+    try{
       return super.add(value);
+    }
+    catch(IllegalArgumentException e){
+      System.out.println("Error occur with adding null.");
+      return false;
     }
   }
 
   public void add(int index, T value){
-    if (value.equals(null)) {
+    if (value == null) {
       throw new IllegalArgumentException("Error occur, check the input, no null is allowed");
     }
-    else{
+    try{
       super.add(index,value);
+    }
+    catch(IllegalArgumentException e){
+      System.out.println("Error occur with adding null.");
     }
   }
 }
