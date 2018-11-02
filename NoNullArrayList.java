673925d286199f8f4fd3c1f2441cb64495136761
ElizaHlglh <ewu4@stuy.edu>
2018-11-01 23:21:14 -0400

    NoNullArrayList.java

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,8 +6,14 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(int startingCapacity){
     super(startingCapacity);
   }
-  public T set(int index, T value) {
 
+  public T set(int index, T value) {
+    if (value.equals(null)) {
+      throw new IllegalArgumentException("Can't set an element as null");
+    }
+    else {
+      return super.set(index,value);
+    }
   }
 
   public boolean add(T value){
@@ -15,14 +21,13 @@ public class NoNullArrayList<T> extends ArrayList<T>{
       throw new IllegalArgumentException("Can't add null");
     }
     else{
-      super.add(value);
-      return true;
+      return super.add(value);
     }
   }
 
   public void add(int index, T value){
-    if (value.equals(null) || index < super.size()) {
-      throw new IllegalArgumentException("Error occur, check index or the input");
+    if (value.equals(null)) {
+      throw new IllegalArgumentException("Error occur, check the input, no null is allowed");
     }
     else{
       super.add(index,value);
